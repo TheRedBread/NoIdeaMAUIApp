c98ce2a8230a337c48b0749941e94d7401dc432e
RedBread <trefel.yt@gmail.com>
2026-09-21 09:36:05 +0200

    init

---
+++ NoIdeaMAUIApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NoIdeaMAUIApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NoIdeaMAUIApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NoIdeaMAUIApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
